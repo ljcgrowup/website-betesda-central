bede5db73701e656ecd0d7459880f77b288ad191
Rafael <herminathor@gmail.com>
2020-07-25 18:38:08 -0300

    scroll pronto

--- js/interaction.js
+++ js/interaction.js
@@ -2,12 +2,26 @@ $(() => {
     $('.scroll').click(e => {
         e.preventDefault();
 
-        let id = e.target.href.substring(e.target.href.indexOf('#')),
-        targetOffset = $(id).offset().top,
-        menuHeight = $('#menu').innerHeight();
+        let id = e.target.href.substring(e.target.href.indexOf('#'));
+        let targetOffset = 0;
+        let menuHeight = 0;
 
-        $('html, body').animate({
-            scrollTop: targetOffset - menuHeight
-        }, 500);
+        if (window.innerWidth <= 767.98) {
+            $('#main-nav').collapse('hide');
+
+            targetOffset = $(id).offset().top;
+            menuHeight = $('#menu').innerHeight();
+
+            $('html, body').animate({
+                scrollTop: targetOffset - 89
+            }, 500);
+        } else {
+            targetOffset = $(id).offset().top;
+            menuHeight = $('#menu').innerHeight();
+
+            $('html, body').animate({
+                scrollTop: targetOffset - menuHeight
+            }, 500);
+        }
     });
 });
